eb414632d4b4ab1779401d530c7323ca54d8cbcd
Nikhil Francis <nikhilfrancis2001@gamil.com>
2025-01-23 12:05:23 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App nothing</h1>
+        <h1>Welcome to My React App for github actions</h1>
         <p>This app is deployed using GitHub Actions.</p>
         <button>Login</button>
       </header>
